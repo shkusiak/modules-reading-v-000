8b27ec090375361c6e07721ca1d51a5470cddd4a
Shea Kusiak <shkusiak@gmail.com>
2018-04-17 02:39:08 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,6 +4,14 @@ require_relative './fancy_dance.rb'
 
 
 class Dancer
+  include Dance
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+  
   extend MetaDancing
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
